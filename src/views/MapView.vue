7e653a8d1a70c99bc1b4d8166e1c9d5c217d3b17
f <f>
2022-04-12 11:46:22 +0200

    first commit

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,28 +1,93 @@
 <template>
-  <l-map style="height: 300px" :zoom="zoom" :center="center">
-    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-    <l-marker :lat-lng="markerLatLng"></l-marker>
+  <l-map
+      class="map"
+      ref="map"
+      :zoom="zoom"
+      :center="center"
+      @update:zoom="zoomUpdated"
+      @update:center="centerUpdated"
+      @update:bounds="boundsUpdated"
+  >
+    <l-marker
+        v-for="(marker, index) in markers"
+        :key="index"
+        :lat-lng="marker.coordinates"
+    >
+      <l-tooltip :content="marker.name" />
+    </l-marker>
+    <l-tile-layer
+        :url="url"
+    >
+    </l-tile-layer>
   </l-map>
 </template>
 
 <script>
-import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
+
+import { LMap, LTileLayer, LMarker, LTooltip, LPopup } from "vue2-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.3.4/dist/images/";
 
 export default {
+  name: 'MapView',
   components: {
-    LMap,
-    LTileLayer,
-    LMarker
+    "l-map": LMap,
+    "l-tile-layer": LTileLayer,
+    "l-marker": LMarker,
+    "l-tooltip": LTooltip,
+    "l-popup": LPopup
   },
   data () {
     return {
-      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
       attribution:
-          '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      zoom: 15,
-      center: [51.505, -0.159],
-      markerLatLng: [51.504, -0.159]
-    };
+          '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      center: [ 44.837789, -0.57918 ],
+      zoom: 5,
+      markers: [
+        {id: 1, name: 'Bordeaux', coordinates: { lat: 44.837789, lng: 	-0.57918 }},
+        {id: 2, name: 'Annecy', coordinates: { lat: 45.8615248, lng: 6.1673232 }},
+        {id: 3, name: 'Londre', coordinates: { lat: 51.509093, lng: -0.094151 }},
+        {id: 4, name: 'Rome', coordinates: { lat: 41.902784, lng: 12.496366 }},
+        {id: 5, name: 'Valence', coordinates: { lat: 44.933393, lng: 4.89236 }},
+      ],
+      markerObjects: null
+    }
+  },
+  mounted: function() {
+    L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.3.4/dist/images/";
+    this.$nextTick(() => {
+      this.markerObjects = this.$refs.markersRef.map(ref => ref.mapObject);
+    });
+  },
+  methods: {
+    displayTooltip(selectedIndex) {
+      for (let markerObject of this.markerObjects) {
+        markerObject.closeTooltip();
+      }
+      this.markerObjects[selectedIndex].toggleTooltip();
+    },
+    zoomUpdated (zoom) {
+      this.zoom = zoom;
+      console.log(this.markers)
+    },
+    centerUpdated (center) {
+      this.center = center;
+    },
+    boundsUpdated (bounds) {
+      this.bounds = bounds;
+    }
   }
 }
 </script>
+
+<style>
+.map {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  overflow :hidden
+}
+</style>
